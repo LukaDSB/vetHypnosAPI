c3c62073f434e7dea7a52beb017bd55046fdfe1f
Walyson Henrique <walysonhenriqueozoriosobrinho@gmail.com>
2025-01-28 17:58:16 -0300

    Testando PR sem criar um fork

--- index.php
+++ index.php
@@ -3,35 +3,31 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 require_once 'controllers/UsuarioController.php';
-require_once 'dto/Usuario.php';
+require_once 'controllers/PacienteController.php';
 
 $request = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
-$usuarioController = new UsuarioController();
+$controllerUsuario = new UsuarioController();
+$controllerPaciente = new PacienteController();
 
 switch ($request) {
     case '/minhaapi/usuarios':
         if ($method === 'POST') {
-            try {
-                $dados = json_decode(file_get_contents("php://input"), true);
-                $usuarioController->createUser($usuario = Usuario::fromArray($dados));
-            } catch (Exception $e) {
-                http_response_code(400);
-                echo json_encode(["message" => $e->getMessage()]);
-            }
+            $data = json_decode(file_get_contents("php://input"), true);
+            $controllerUsuario->createUser($data);
         } elseif ($method === 'GET') {
-            $usuarioController->getAllUsers();
+            $controllerUsuario->getAllUsers();
         }
         break;
-        
-    case (preg_match('/^\/minhaapi\/usuarios\/\d+$/', $request) ? true : false):
-        if ($method === 'GET') {
-            $id = basename($request);
-            $usuarioController->getById($id);
-        }
+        case '/minhaapi/pacientes':
+            if ($method === 'POST') {
+                $data = json_decode(file_get_contents("php://input"), true);
+                $controllerPaciente->createPaciente($data);
+            } elseif ($method === 'GET') {
+                $controllerPaciente->getAllPacientes();
+            }
         break;
-
     default:
         http_response_code(404);
         echo json_encode(["message" => "Rota não encontrada."]);
