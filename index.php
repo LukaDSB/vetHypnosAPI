bb263e010087bf6b85b8cece469336402e694e65
= <=>
2025-03-09 17:23:36 -0300

    Final, index atualizado

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ header("Content-Type: application/json; charset=UTF-8");
 
 require_once 'controllers/UsuarioController.php';
 require_once 'controllers/PacienteController.php';
+require_once 'controllers/MedicamentoController.php';
 
 $request = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
 $controllerUsuario = new UsuarioController();
 $controllerPaciente = new PacienteController();
+$controllerMedicamento = new MedicamentoController();
 
 switch ($request) {
     case '/minhaapi/usuarios':
@@ -28,7 +30,22 @@ switch ($request) {
                 $controllerPaciente->getAllPacientes();
             }
         break;
+        case '/minhaapi/medicamentos':
+            if ($method === 'POST'){
+                $data = json_decode(file_get_contents("php://input"), true);
+                $controllerMedicamento->createMedicamento($data);
+            } elseif ($method === 'GET') {
+                $controllerMedicamento->getAllMedicamentos();
+            }elseif ($method === 'DELETE') {
+                $controllerMedicamento->deleteMedicamento();
+            }elseif ($method === 'PUT') {
+                $data = json_decode(file_get_contents("php://input"), true);
+                $controllerMedicamento->updateMedicamento($data);
+            }
+            
+            break;
     default:
+
         http_response_code(404);
         echo json_encode(["message" => "Rota não encontrada."]);
 }
