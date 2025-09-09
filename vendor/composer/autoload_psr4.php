8227d5348e9b31b2115b6c1a1810a9c7b128ef7c
Lucas Benevenute <souzalucas6@hotmail.com>
2025-09-09 08:13:08 -0300

    Remover a pasta vendor do versionamento ;D

--- vendor/composer/autoload_psr4.php
+++
@@ -1,8 +0,0 @@
-<?php
-$vendorDir = dirname(__DIR__);
-$baseDir = dirname($vendorDir);
-
-return array(
-    'Firebase\\JWT\\' => array($vendorDir . '/firebase/php-jwt/src'),
-    'App\\' => array($baseDir . '/src'),
-);
